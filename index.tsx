ef6a10eaa82744cff36fbe5c6a2e1dae9f18bcd2
Tutuhuang <38522452+tutusiji@users.noreply.github.com>
2025-12-01 02:01:59 +0800

    feat: set up Tailwind CSS and integrate custom fonts

--- index.tsx
+++ index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "@fontsource/inter";
+import "@fontsource/press-start-2p";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
